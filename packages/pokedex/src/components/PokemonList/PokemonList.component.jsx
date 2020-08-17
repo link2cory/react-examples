a967e4e38cd3d7a6f9a38f167a1959cf8e669305
Cory Perkins <link2cory@gmail.com>
2020-08-17 10:04:10 -0700

    add infinite loading to PokemonList

--- packages/pokedex/src/components/PokemonList/PokemonList.component.jsx
+++ packages/pokedex/src/components/PokemonList/PokemonList.component.jsx
@@ -1,29 +1,42 @@
 import React from "react";
-import useSwr from "swr";
+import { useSWRInfinite } from "swr";
 
 import Pokemon from "../Pokemon";
 
 import { PokemonListContainer } from "./PokemonList.styles";
 
-const fetcher = (url) => fetch(url).then((r) => r.json());
+const POKEMON_PER_LOAD = 20;
 
 const PokemonList = ({}) => {
-  const key = "https://pokeapi.co/api/v2/pokemon/";
-  const { data: pokemonList, error } = useSwr(key, fetcher);
+  const { data, error, size, setSize } = useSWRInfinite(
+    (index) => {
+      console.log(index);
+      return `https://pokeapi.co/api/v2/pokemon?offset=${
+        index * POKEMON_PER_LOAD
+      }&limit=${POKEMON_PER_LOAD}`;
+    },
+    (url) => fetch(url).then((r) => r.json())
+  );
 
   if (error) {
     return <div>Error fetching Pokemon List</div>;
   }
 
-  if (!pokemonList) {
+  if (!data) {
     return <div>Loading Pokemon List</div>;
   }
 
   return (
     <PokemonListContainer>
-      {pokemonList.results.map((pokemon) => (
-        <Pokemon id={pokemon.name} />
-      ))}
+      {data.map((pokemonLoad) =>
+        pokemonLoad.results.map((pokemon) => <Pokemon id={pokemon.name} />)
+      )}
+      <button
+        onClick={() => setSize(size + 1)}
+        style={{ width: "100%", height: "100%" }}
+      >
+        Load More
+      </button>
     </PokemonListContainer>
   );
 };
